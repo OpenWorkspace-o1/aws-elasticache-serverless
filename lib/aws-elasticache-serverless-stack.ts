82a609306b17de94bed60addc4815b6958c29949
Trung Dinh <xamarindevelopervietnam@outlook.com>
2024-12-07 17:35:36 +0700

    Add interface AwsElasticacheServerlessStackProps

--- lib/aws-elasticache-serverless-stack.ts
+++ lib/aws-elasticache-serverless-stack.ts
@@ -1,8 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { AwsElasticacheServerlessStackProps } from './AwsElasticacheServerlessStackProps';
 
 export class AwsElasticacheServerlessStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsElasticacheServerlessStackProps) {
     super(scope, id, props);
 
   }
